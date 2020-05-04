31c7394f29225a6c8af697ac2c749b902386ec8a
Franco Losardo <francolosa@hotmail.com>
2020-05-04 01:27:19 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import speedtest
+
+
+st = speedtest.Speedtest()
+option = input("""Choose below which speed you want to test
+1 Download Speed
+2 Upload
+3 Ping
+Option: """)
+
+
+def sptest():
+    if option == 1:
+        st.get_best_server()
+        print(round(st.download() / 8e+6, 2), "Mb/s")
+    elif option == 2:
+        st.get_best_server()
+        print(round(st.upload() / 8e+6, 2), "Mb/s")
+    elif option == 3:
+        servernames = []
+        st.get_best_server()
+        st.get_servers(servernames)
+        print(round(st.results.ping), "ms")
+    else:
+        print("Wrong option, please try again")
+
+
+sptest()
